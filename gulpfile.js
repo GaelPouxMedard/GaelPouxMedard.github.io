8f2a8c68836b0d60dec223376b91d6b874fadcda
GaelPouxMedard <61467946+GaelPouxMedard@users.noreply.github.com>
2021-09-22 15:47:03 +0200

    Changes

--- gulpfile.js
+++ gulpfile.js
@@ -75,55 +75,28 @@ gulp.task('default', ['browser-sync', 'watch']);
 
 
 // Theme transition clics
-$(document).ready(function() {
-  var ripple_wrap = $('.ripple-wrap'),
-      rippler = $('.ripple'),
-      finish = false,
-      monitor = function(el) {
-        var computed = window.getComputedStyle(el, null),
-            borderwidth = parseFloat(computed.getPropertyValue('border-left-width'));
-        if (!finish && borderwidth >= 1500) {
-          el.style.WebkitAnimationPlayState = "paused";
-          el.style.animationPlayState = "paused";
-          swapContent();
-        }
-        if (finish) {
-          el.style.WebkitAnimationPlayState = "running";
-          el.style.animationPlayState = "running";
-          return;
-        } else {
-          window.requestAnimationFrame(function() {monitor(el)});
-        }
-      };
 
-  storedcontent = $('#content-2').html();
-  $('#content-2').remove();
 
-  rippler.bind("webkitAnimationEnd oAnimationEnd msAnimationEnd mozAnimationEnd animationend", function(e){
-    ripple_wrap.removeClass('goripple');
+var $cont = document.querySelector('.cont');
+var $elsArr = [].slice.call(document.querySelectorAll('.el'));
+var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
+
+setTimeout(function() {
+  $cont.classList.remove('s--inactive');
+}, 200);
+
+$elsArr.forEach(function($el) {
+  $el.addEventListener('click', function() {
+    if (this.classList.contains('s--active')) return;
+    $cont.classList.add('s--el-active');
+    this.classList.add('s--active');
   });
+});
 
-  $('body').on('click', 'a', function(e) {
-    rippler.css('left', e.clientX + 'px');
-    rippler.css('top', e.clientY + 'px');
-    e.preventDefault();
-    finish = false;
-    ripple_wrap.addClass('goripple');
-    window.requestAnimationFrame(function() {monitor(rippler[0])});
-
-
+$closeBtnsArr.forEach(function($btn) {
+  $btn.addEventListener('click', function(e) {
+    e.stopPropagation();
+    $cont.classList.remove('s--el-active');
+    document.querySelector('.el.s--active').classList.remove('s--active');
   });
-
-
-
-  function swapContent() {
-      var newcontent = $('#content-area').html();
-      $('#content-area').html(storedcontent);
-      storedcontent = newcontent;
-      // do some Ajax, put it in the DOM and then set this to true
-      setTimeout(function() {
-        finish = true;
-      },10);
-  }
-
 });
